18a9030c0542d3309addfe3423631a9e58cde5c1
Michael Cuccia <michael.cuccia@egplusww.com>
2023-08-23 16:18:21 -0400

    ToDo create Context

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # applicazione
 
 nvm use 18 - https://github.com/nvm-sh/nvm
+
+visit - https://applicazione.vercel.app
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,19 @@
-import styles from './page.module.scss'
+"use client"
+import styles from './page.module.scss';
 
 export default function Home() {
+
+  const handleClick = () => {
+    console.log('click');
+  }
+
   return (
     <>
       <div className={styles.main}>
         hello world
+      </div>
+      <div onClick={handleClick}>
+        click to set
       </div>
     </>
   )
