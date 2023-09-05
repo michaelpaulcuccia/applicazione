9fac23932d5d3e3680e3a258c4a4d04079765359
Michael Cuccia <michael.cuccia@egplusww.com>
2023-09-05 10:10:19 -0400

    updates login and adds some css

--- client/src/app/components/CheckContext.jsx
+++ client/src/app/components/CheckContext.jsx
@@ -1,18 +1,44 @@
 "use client"
-import React,{ useContext}  from 'react';
+import React,{ useState, useContext}  from 'react';
 import UserContext from '../context/UserContext';
 
 export default function CheckContext() {
 
   const { setContextUser } = useContext(UserContext);
 
-  const handleClick = () => {
-    setContextUser({userName: 'michael', age: 43});
+  const [inputUserName, setInputUserName] = useState('')
+  const [inputAge, setInputAge] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setContextUser({ inputUserName, inputAge })
+    //clears form
+    setInputUserName('');
+    setInputAge('')
   }
 
   return (
-    <div onClick={handleClick}>
-        click to set
+    <div>
+      <form 
+        onSubmit={handleSubmit}
+        className='input_hide_number_scroll_arrows'
+      >
+        <div>
+          <label htmlFor='inputUserName'>Name</label>
+            <input 
+              type='text' 
+              value={inputUserName} 
+              onChange={(event) => setInputUserName(event.target.value)} 
+            />
+            <label htmlFor='inputAge'>Age</label>
+            <input 
+              type='number' 
+              value={inputAge} 
+              onChange={(event) => setInputAge(event.target.value)} 
+            />
+        </div>
+        <input type='submit' value='Login' />
+      </form>
     </div>
   )
 }
